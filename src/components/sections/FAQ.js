d9e0ccd1a31324592c0f5ea4129692e0e8cce657
marcelmos <kangurexplay@gmail.com>
2021-12-29 14:21:08 +0100

    FAQ section

--- src/components/sections/FAQ.js
+++ src/components/sections/FAQ.js
@@ -1,20 +1,60 @@
 
 
 export default function FAQ(){
+    // Element data
+    const faqData = [
+        {
+            question: 'Question 1',
+            answear: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam ratione alias accusantium cum. Aut illum eius ipsam totam inventore maiores quod dolor voluptates, vero, quos consectetur nihil quidem officia? Aliquid.',
+        },
+        {
+            question: 'Question 2',
+            answear: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam ratione alias accusantium cum. Aut illum eius ipsam totam inventore maiores quod dolor voluptates, vero, quos consectetur nihil quidem officia? Aliquid.',
+        },
+        {
+            question: 'Question 3',
+            answear: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam ratione alias accusantium cum. Aut illum eius ipsam totam inventore maiores quod dolor voluptates, vero, quos consectetur nihil quidem officia? Aliquid.',
+        },
+    ]
+    // Element logic
+    const onShow = (index) => {
+        const div = document.querySelector(`.faq-answear[data-key='answear-${index}']`);
+
+        if(div.classList.contains('show')){
+            div.classList.remove('show');
+        }else{
+            div.classList.add('show');
+        }
+    }
 
 
-
+    // Render Section
     return(
         <section id="faq-section" className="py-20 px-24">
             <h2 className='section-header'>FAQ</h2>
 
-            <div className="grid grid-cols-3 gap-7 mt-8">
+            <div className="mt-8">
 
-                <div className="faq-wrapper">
-                    <div className="faq-question" data-label="question-1" onClick={console.log(document.querySelector('.faq-answear'))}>Question 1</div>
-                    <div className="faq-answear" data-label="answear-1">Answear 1</div>
-                    <div className="faq-question" data-label="question-2" onClick={console.log(document.querySelector('.faq-answear'))}>Question 2</div>
-                    <div className="faq-answear" data-label="answear-2">Answear 2</div>
+                <div className="faq-wrapper mx-auto">
+                    { faqData.map((el, index) => {
+                            return(
+                                <>
+                                    <div className="faq-question"
+                                    onClick={(event) =>{
+                                        onShow(index);
+                                        console.log(event.target);
+                                        if(event.target.classList.contains('opend')){
+                                            event.target.classList.remove('opend');
+                                        }else{
+                                            event.target.classList.add('opend');
+                                        }
+                                     }}>
+                                        { el.question }
+                                    </div>
+                                    <div className="faq-answear" data-key={`answear-${index}`}>{ el.answear }</div>
+                                </>
+                             );
+                        }) }
                 </div>
 
             </div>
